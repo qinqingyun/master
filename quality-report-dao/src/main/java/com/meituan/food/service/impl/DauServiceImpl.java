0a712b10ee24ba394f40eb8ada1577cde351c069
郭孟瑶 <guomengyao@meituan.com>
2018-11-18 20:06:10 +0800

    fix

--- quality-report-dao/src/main/java/com/meituan/food/service/impl/DauServiceImpl.java
+++ quality-report-dao/src/main/java/com/meituan/food/service/impl/DauServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class DauServiceImpl implements IDauService {
 
     private final static Logger log=LoggerFactory.getLogger(DauServiceImpl.class);
