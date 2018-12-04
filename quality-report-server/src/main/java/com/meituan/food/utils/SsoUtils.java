3f0e1f4583509277226f350e3d84abd5e7339a50
郭孟瑶 <guomengyao@meituan.com>
2018-12-04 15:48:33 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/utils/SsoUtils.java
+++ quality-report-server/src/main/java/com/meituan/food/utils/SsoUtils.java
@@ -1,8 +1,11 @@
 package com.meituan.food.utils;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.ImmutableMap;
 
+import java.net.URI;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -29,7 +32,46 @@ public final class SsoUtils {
                 Collections.emptyMap());
         return tokenRes.getString("access_token");*/
 
-       return "a3e8a0911a*84b069e6b16d2dd1daef4";
+       //线上sso_host
+        String sso_host="https://sso.vip.sankuai.com/sson";
+        //线下sso_host
+       // String sso_host="http://ssosv.it.beta.sankuai.com/sson";
+
+        //线上clientId
+        String clientId="0d6cf99067";
+        //线上AppSecret
+        String appSecret="e77b663737ae4a938b0673818a0e68ad";
+
+        //线下clientId
+      //  String clientId="cc6049a1ee";
+        //线下AppSecret
+       // String appSecret="55777cf8eda44eaca7ffeedcc806d246";
+
+        JSONObject authParams=new JSONObject();
+        authParams.put("loginName","test_meishi_qa");
+       authParams.put("password","meishi_qa1234");
+      // authParams.put("password","Ceshi123");
+       authParams.put("isSingleDevice",false);
+
+       JSONObject authRes=HttpUtils.doPostWithAuth(URI.create(sso_host+"/api/auth"),
+               authParams.toJSONString(),
+               JSONObject.class,
+               clientId,appSecret);
+       String refreshToken=authRes.getJSONObject("data").getString("refreshToken");
+
+       JSONObject authParam=new JSONObject();
+       authParam.put("refreshToken",refreshToken);
+       JSONObject ssoRes=HttpUtils.doPostWithAuth(URI.create(sso_host+"/oauth2.0/refresh-token"),
+               authParam.toJSONString(),
+               JSONObject.class,
+               clientId,appSecret);
+
+       String ssoId=ssoRes.getJSONObject("data").getString("accessToken");
+
+       return ssoId;
+
+
+     //  return "a3e8a0911a*84b069e6b16d2dd1daef4";
     }
 
     private SsoUtils() {
