5fad8c8378a2804af0690e3804e520dd1bf4ccae
郭孟瑶 <guomengyao@meituan.com>
2019-01-02 14:30:59 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/web/OrgTestController.java
+++ quality-report-server/src/main/java/com/meituan/food/web/OrgTestController.java
@@ -1,8 +1,12 @@
 package com.meituan.food.web;
 
+import com.sankuai.meituan.org.openapi.model.EmpCond;
 import com.sankuai.meituan.org.opensdk.model.domain.Emp;
+import com.sankuai.meituan.org.opensdk.model.domain.items.EmpItems;
 import com.sankuai.meituan.org.opensdk.service.EmpService;
+import com.sankuai.meituan.org.queryservice.domain.base.Paging;
 import com.sankuai.meituan.org.queryservice.exception.MDMThriftException;
+import com.sankuai.meituan.org.treeservice.domain.EmpHierarchyCond;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,5 +26,19 @@ public class OrgTestController {
         String stringEmp=emp.toString();
 
         return stringEmp;
+    }
+
+    @GetMapping
+    public String testOrgIdController(@RequestParam("orgId") String orgId)throws MDMThriftException{
+
+        EmpHierarchyCond empCond = new EmpHierarchyCond();
+        empCond = empCond.jobStatusIdET(15);//在职
+        Paging paging = new Paging();
+        paging.setSize(100);
+
+        EmpItems empItems = empService.queryEmp(orgId, 2, empCond, paging);
+        String stringEmpItems=empItems.toString();
+
+        return stringEmpItems;
     }
 }
--- quality-report-server/src/main/java/com/meituan/food/web/vo/OrgTestVO.java
+++ quality-report-server/src/main/java/com/meituan/food/web/vo/OrgTestVO.java
@@ -1,4 +1,13 @@
 package com.meituan.food.web.vo;
 
+import lombok.Data;
+
+@Data
 public class OrgTestVO {
+    private String empId;
+    private String name;
+    private String misId;
+    private String reportMis;
+    private String orgName;
+    private String orgId;
 }
