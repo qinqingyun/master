19f42b9d8a49ddf7a1b55288074b909acc90b579
郭孟瑶 <guomengyao@meituan.com>
2019-01-04 10:58:14 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/web/TestController.java
+++ quality-report-server/src/main/java/com/meituan/food/web/TestController.java
@@ -1,7 +1,5 @@
 package com.meituan.food.web;
 
-import com.meituan.food.mapper.RestaurantXueChengPOMapper;
-import com.meituan.food.po.RestaurantXueChengPO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,17 +10,17 @@ import javax.annotation.Resource;
 @RestController
 public class TestController {
 
-    @Resource
-    private RestaurantXueChengPOMapper restaurantXueChengPOMapper;
+   /* @Resource
+    private RestaurantXueChengPOMapper restaurantXueChengPOMapper;*/
 
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
         return "hello world," + name;
     }
 
-    @GetMapping("/xuecheng")
+  /*  @GetMapping("/xuecheng")
     public String xuechengEx(@RequestParam("mis") String mis){
         RestaurantXueChengPO restaurantXueChengPO = restaurantXueChengPOMapper.selectByPrimaryMis(mis);
         return restaurantXueChengPO.toString();
-    }
+    }*/
 }
