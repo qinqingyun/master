637d636031a537fa6942759e417b4caccc09fdb5
郭孟瑶 <guomengyao@meituan.com>
2019-01-17 11:05:29 +0800

    test

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/TaskDurationExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/TaskDurationExtracter.java
@@ -99,7 +99,7 @@ public class TaskDurationExtracter  implements IOneDayFourteenExtract {
                 }
             }
 
-            EmpHierarchyCond empCond = new EmpHierarchyCond();
+          /*  EmpHierarchyCond empCond = new EmpHierarchyCond();
             empCond = empCond.jobStatusIdET(15);//在职
             Paging paging = new Paging();
             paging.setSize(1000);
@@ -138,7 +138,7 @@ public class TaskDurationExtracter  implements IOneDayFourteenExtract {
                     taskDurationPOMapper.insert(po);
                 }
             }
-        }
+        }*/
 
         for (TaskDurationPO po : taskDurationPOS) {
             if(po.getIsnormal()==false){
