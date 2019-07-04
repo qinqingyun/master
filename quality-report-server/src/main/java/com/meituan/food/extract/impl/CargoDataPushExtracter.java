a193d039ad9b450be1824c1b3e78fe9db819253a
guomengyao <guomengyao@meituan.com>
2019-07-04 15:28:32 +0800

    修复cargo数据推送时的空指针问题

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/CargoDataPushExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/CargoDataPushExtracter.java
@@ -51,7 +51,8 @@ public class CargoDataPushExtracter implements ICargoDataPushExtract {
         String yesterdayStr = new SimpleDateFormat("yyyy-MM-dd").format(yesterday.getTime());
         Date yesterdayDate = yesterday.getTime();
 
-        Date mYesterday = Date.from(LocalDate.now().minusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date mYesterday = Date.from(LocalDate.now().minusDays(2).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        java.sql.Date sqlDate=new java.sql.Date(mYesterday.getTime());
 
 
         String stableList = yesterdayStr + "稳定性数据\n";
@@ -63,23 +64,29 @@ public class CargoDataPushExtracter implements ICargoDataPushExtract {
         int flag_2 = 0;
         String bAvalibleList = yesterdayStr + "可用性数据\n";
         int flag_3 = 0;
-        List<CargoDataPO> cargoDataPOS = cargoDataPOMapper.selectByDate(mYesterday);
+        List<CargoDataPO> cargoDataPOS = cargoDataPOMapper.selectByDate(sqlDate);
 
         for (CargoDataPO cargoDataPO : cargoDataPOS) {
-            stableList = stableList + cargoDataPO.getDirection() + "_" + cargoDataPO.getTag() + ":" + cargoDataPO.getStableTagPercentage() + "\n";
-            flag++;
-            if (cargoDataPO.getDirection().equals("B端_北京") || cargoDataPO.getDirection().equals("B端_上海")) {
-                bStableList = bStableList + cargoDataPO.getDirection() + "_" + cargoDataPO.getTag() + ":" + cargoDataPO.getStableTagPercentage() + "\n";
-                flag_1++;
+            if (cargoDataPO.getDirection()!=null) {
+                stableList = stableList + cargoDataPO.getDirection() + "_" + cargoDataPO.getTag() + ":" + cargoDataPO.getStableTagPercentage() + "\n";
+                flag++;
+                if (cargoDataPO.getDirection().equals("B端_北京") || cargoDataPO.getDirection().equals("B端_上海")) {
+                    bStableList = bStableList + cargoDataPO.getDirection() + "_" + cargoDataPO.getTag() + ":" + cargoDataPO.getStableTagPercentage() + "\n";
+                    flag_1++;
+                }
+            }else {
+                stableList = stableList +  cargoDataPO.getTag() + ":" + cargoDataPO.getStableTagPercentage() + "\n";
             }
 
-            if (cargoDataPO.getAvalibleTotal() != 0) {
+            if (cargoDataPO.getAvalibleTotal() != 0 &&cargoDataPO.getDirection()!=null) {
                 avalibleList = avalibleList + cargoDataPO.getDirection() + "_" + cargoDataPO.getTag() + ":" + cargoDataPO.getAvalibleTagPercentage() + "\n";
                 flag_2++;
                 if (cargoDataPO.getDirection().equals("B端_北京") || cargoDataPO.getDirection().equals("B端_上海")) {
                     bAvalibleList = bAvalibleList + cargoDataPO.getDirection() + "_" + cargoDataPO.getTag() + ":" + cargoDataPO.getAvalibleTagPercentage() + "\n";
                     flag_3++;
                 }
+            }else if (cargoDataPO.getAvalibleTotal() != 0){
+                avalibleList = avalibleList + cargoDataPO.getTag() + ":" + cargoDataPO.getAvalibleTagPercentage() + "\n";
             }
         }
 
