7ecceafd56b390c2f985372b767a99f03a0eb32c
贾晓琪 <jiaxiaoqi@meituan.com>
2019-07-18 17:46:21 +0800

    增加resultStatus=2的状态

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/GetLineCoverageExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/GetLineCoverageExtracter.java
@@ -87,93 +87,102 @@ public class GetLineCoverageExtracter implements IGetLineCoverageExtract {
             int resultStatus;
 
             // 最外层code，1(fail), 直接退出
-            // 最外层code，0，继续。
+            // 最外层code，0，继续：
             //       内侧状态码，preparationStatus 2 轮询（任务执行中）
-            //                 preparationStatus 其他值 错误退出
-            //                 preparationStatus 不存在：判断resultStatus 0正常 1异常
+            //                 preparationStatus 非2 错误退出
+            //                 preparationStatus 不存在：判断resultStatus 0正常 2轮询 非0且非2异常
             if (code == 0) {
-                // 内侧状态码preparationStatus存在：2继续轮序 其他值退出
-                while (resp.getJSONObject("data").containsKey("preparationStatus")) {
-                    preparationStatus = resp.getJSONObject("data").getInteger("preparationStatus");
-                    po.setErrorMessage("preparationStatus="+preparationStatus);
-
-                    if (preparationStatus == 2) {
-                        resp = HttpUtils.doGet(urlF,JSONObject.class, ImmutableMap.of());
-                    } else {
-                        insterPO(po);
-                        break;
-                    }
-                }
-                // 内侧状态码preparationStatus不存在：判断resultStatus 0正常 1异常
-                // preparationStatus和resultStatus应该不会同时存在吧？暂时没做处理，默认只存在一个
-                if (resp.getJSONObject("data").containsKey("resultStatus")) {
-                    resultStatus = resp.getJSONObject("data").getInteger("resultStatus");
-
-                    if (resultStatus == 0) {
-                        // 核心接口覆盖行数
-                        int coreLineC = resp.getJSONObject("data").getJSONObject("coreData").getInteger("lineC");
-                        // 核心接口未覆盖行数
-                        int coreLineM = resp.getJSONObject("data").getJSONObject("coreData").getInteger("lineM");
-                        // 核心接口总行数
-                        int coreLineTotal = coreLineC + coreLineM;
-                        // 计算核心接口行覆盖率（四舍五入BigDecimal.ROUND_HALF_U）
-                        if (coreLineTotal != 0) {
-                            BigDecimal coreLineCoverage = (new BigDecimal(coreLineC)).divide (new BigDecimal(coreLineTotal),4,BigDecimal.ROUND_HALF_UP);
-                            coreLineCoverage = coreLineCoverage.movePointRight(2);
-                            po.setCoreLineCoverage(coreLineCoverage);
-                        } else {
-                            po.setCoreLineCoverage(BigDecimal.valueOf(0));
-                        }
-                        // 核心接口行覆盖率（接口获取，string，直接丢弃未四舍五入）
-                        String coreLineCoverageInterface = resp.getJSONObject("data").getJSONObject("coreData").getString("lineCoverage");
-                        // 全部接口覆盖行数
-                        int totalLineC = resp.getJSONObject("data").getJSONObject("totalData").getInteger("lineC");
-                        // 全部接口未覆盖行数
-                        int totalLineM = resp.getJSONObject("data").getJSONObject("totalData").getInteger("lineM");
-                        // 全部接口总行数
-                        int totalLineTotal = totalLineC + totalLineM;
-                        // 计算全部接口行覆盖率（四舍五入BigDecimal.ROUND_HALF_U）
-                        if (totalLineTotal != 0) {
-                            BigDecimal totalLineCoverage = (new BigDecimal(totalLineC)).divide (new BigDecimal(totalLineTotal),4,BigDecimal.ROUND_HALF_UP);
-                            totalLineCoverage = totalLineCoverage.movePointRight(2);
-                            po.setTotalLineCoverage(totalLineCoverage);
-                        } else {
-                            po.setTotalLineCoverage(BigDecimal.valueOf(0));
-                        }
-                        // 全部接口行覆盖率（接口获取，string，直接丢弃未四舍五入）
-                        String totalLineCoverageInterface = resp.getJSONObject("data").getJSONObject("totalData").getString("lineCoverage");
-
-                        po.setReleaseName(releaseName);
-                        po.setSrv(srv);
-                        po.setCoreLineC(coreLineC);
-                        po.setCoreLineM(coreLineM);
-                        po.setCoreLineTotal(coreLineTotal);
-                        // po.setTotalLineCoverage(totalLineCoverage);放在if判断中
-                        po.setCoreLineCInterface(coreLineCoverageInterface);
-                        po.setTotalLineC(totalLineC);
-                        po.setTotalLineM(totalLineM);
-                        po.setTotalLineTotal(totalLineTotal);
-                        // po.setTotalLineCoverage(totalLineCoverage);放在if判断中
-                        po.setTotalLineCInterface(totalLineCoverageInterface);
-                        po.setDepartmentId(departmentId);
-                        po.setDepartmentName(departmentName);
-                        po.setDepartmentId2(departmentId2);
-                        po.setDepartmentName2(departmentName2);
-                        po.setCreatedTime(now);
-                        po.setUpdateTime(now);
-                    } else {
-                        insterPO(po);
-                        po.setErrorMessage("resultStatus="+resultStatus);
-                    }
-                } else {
+                // preparationStatus和resultStatus都不存在：存默认数据，记录错误信息
+                if ((!resp.getJSONObject("data").containsKey("preparationStatus")) && (!resp.getJSONObject("data").containsKey("resultStatus"))) {
                     insterPO(po);
                     po.setErrorMessage("preparationStatus不存在；resultStatus不存在");
+                }
+                // preparationStatus或resultStatus存在，执行循环
+                // 或条件原因：preparationStatus=2或resultStatus=2，都会重新发请求，结果可能有preparationStatus，也可能有resultStatus
+                // break条件：preparationStatus-非2-错误退出；resultStatus-0-完成计算并存数据；resultStatus-非0且非2-错误退出
+                while (resp.getJSONObject("data").containsKey("preparationStatus") || resp.getJSONObject("data").containsKey("resultStatus")) {
+                    // 内侧状态码preparationStatus存在：2继续轮序 其他值退出
+                    if (resp.getJSONObject("data").containsKey("preparationStatus")) {
+                        preparationStatus = resp.getJSONObject("data").getInteger("preparationStatus");
+                        po.setErrorMessage("preparationStatus="+preparationStatus);
+
+                        if (preparationStatus == 2) {
+                            resp = HttpUtils.doGet(urlF,JSONObject.class, ImmutableMap.of());
+                        } else {
+                            insterPO(po);
+                            break;
+                        }
+                    }
+                    // 内侧状态码preparationStatus不存在：判断resultStatus 0正常 1异常 2轮询
+                    if (resp.getJSONObject("data").containsKey("resultStatus")) {
+                        resultStatus = resp.getJSONObject("data").getInteger("resultStatus");
+
+                        if (resultStatus == 0) {
+                            // 核心接口覆盖行数
+                            int coreLineC = resp.getJSONObject("data").getJSONObject("coreData").getInteger("lineC");
+                            // 核心接口未覆盖行数
+                            int coreLineM = resp.getJSONObject("data").getJSONObject("coreData").getInteger("lineM");
+                            // 核心接口总行数
+                            int coreLineTotal = coreLineC + coreLineM;
+                            // 计算核心接口行覆盖率（四舍五入BigDecimal.ROUND_HALF_U）
+                            if (coreLineTotal != 0) {
+                                BigDecimal coreLineCoverage = (new BigDecimal(coreLineC)).divide (new BigDecimal(coreLineTotal),4,BigDecimal.ROUND_HALF_UP);
+                                coreLineCoverage = coreLineCoverage.movePointRight(2);
+                                po.setCoreLineCoverage(coreLineCoverage);
+                            } else {
+                                po.setCoreLineCoverage(BigDecimal.valueOf(0));
+                            }
+                            // 核心接口行覆盖率（接口获取，string，直接丢弃未四舍五入）
+                            String coreLineCoverageInterface = resp.getJSONObject("data").getJSONObject("coreData").getString("lineCoverage");
+                            // 全部接口覆盖行数
+                            int totalLineC = resp.getJSONObject("data").getJSONObject("totalData").getInteger("lineC");
+                            // 全部接口未覆盖行数
+                            int totalLineM = resp.getJSONObject("data").getJSONObject("totalData").getInteger("lineM");
+                            // 全部接口总行数
+                            int totalLineTotal = totalLineC + totalLineM;
+                            // 计算全部接口行覆盖率（四舍五入BigDecimal.ROUND_HALF_U）
+                            if (totalLineTotal != 0) {
+                                BigDecimal totalLineCoverage = (new BigDecimal(totalLineC)).divide (new BigDecimal(totalLineTotal),4,BigDecimal.ROUND_HALF_UP);
+                                totalLineCoverage = totalLineCoverage.movePointRight(2);
+                                po.setTotalLineCoverage(totalLineCoverage);
+                            } else {
+                                po.setTotalLineCoverage(BigDecimal.valueOf(0));
+                            }
+                            // 全部接口行覆盖率（接口获取，string，直接丢弃未四舍五入）
+                            String totalLineCoverageInterface = resp.getJSONObject("data").getJSONObject("totalData").getString("lineCoverage");
+
+                            po.setReleaseName(releaseName);
+                            po.setSrv(srv);
+                            po.setCoreLineC(coreLineC);
+                            po.setCoreLineM(coreLineM);
+                            po.setCoreLineTotal(coreLineTotal);
+                            // po.setTotalLineCoverage(totalLineCoverage);放在if判断中
+                            po.setCoreLineCInterface(coreLineCoverageInterface);
+                            po.setTotalLineC(totalLineC);
+                            po.setTotalLineM(totalLineM);
+                            po.setTotalLineTotal(totalLineTotal);
+                            // po.setTotalLineCoverage(totalLineCoverage);放在if判断中
+                            po.setTotalLineCInterface(totalLineCoverageInterface);
+                            po.setDepartmentId(departmentId);
+                            po.setDepartmentName(departmentName);
+                            po.setDepartmentId2(departmentId2);
+                            po.setDepartmentName2(departmentName2);
+                            po.setCreatedTime(now);
+                            po.setUpdateTime(now);
+                            break;
+                        } else if (resultStatus == 2){
+                            resp = HttpUtils.doGet(urlF,JSONObject.class, ImmutableMap.of());
+                        } else {
+                            insterPO(po);
+                            po.setErrorMessage("resultStatus="+resultStatus);
+                            break;
+                        }
+                    }
                 }
             } else {
                 insterPO(po);
                 po.setErrorMessage("code="+code);
             }
-
             lineCoverageP0Mapper.insert(po);
         }
     }
