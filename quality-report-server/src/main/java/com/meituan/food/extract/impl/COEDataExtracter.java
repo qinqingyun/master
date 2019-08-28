09b71b9218a02694c1cd787e6bf00a652e12c1ed
guomengyao <guomengyao@meituan.com>
2019-08-28 10:31:00 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/COEDataExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/COEDataExtracter.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.ImmutableMap;
 import com.meituan.food.extract.ICOEDataExtract;
 import com.meituan.food.utils.HttpUtils;
+import retrofit2.http.PUT;
 
 public class COEDataExtracter implements ICOEDataExtract {
 
@@ -11,11 +12,16 @@ public class COEDataExtracter implements ICOEDataExtract {
 
     @Override
     public void getCOEData(String dateStr) {
-        String department="dianping.dc";
         JSONObject params=new JSONObject();
-        params.put("occur_start",dateStr);
-        params.put("occur_end",dateStr);
-        params.put("department",department);
+        params.put("occur_start","2019-7-10");
+        params.put("occur_end","2019-8-22");
+        params.put("org",70);
+        params.put("category","");
+        params.put("page",1);
+        params.put("page_size",10);
+        params.put("sort","desc");
+        params.put("sort_by","create_at");
+        params.put("list_type","all");
 
         JSONObject resp=HttpUtils.doPost(url,params.toJSONString(),JSONObject.class,ImmutableMap.of("content-type", "application/json","Accept","text/plain, text/html,application/json","Authorization", "Bearer 9767c6e3488c88135f5032bc6da7339084b1a05b"));
         resp.toJSONString();
