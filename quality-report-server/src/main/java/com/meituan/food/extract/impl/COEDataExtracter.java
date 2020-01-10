189a42c28444fd500ea587ce2714f7a8a18713b1
guomengyao <guomengyao@meituan.com>
2020-01-10 17:34:43 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/COEDataExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/COEDataExtracter.java
@@ -1,6 +1,5 @@
 package com.meituan.food.extract.impl;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.ImmutableMap;
@@ -9,12 +8,7 @@ import com.meituan.food.mapper.CoeListP0Mapper;
 import com.meituan.food.po.CoeListP0;
 import com.meituan.food.utils.DaXiangUtils;
 import com.meituan.food.utils.HttpUtils;
-import com.meituan.food.utils.SsoUtils;
-import org.apache.el.lang.ELArithmetic;
-import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
-import org.yaml.snakeyaml.events.Event;
-import retrofit2.http.PUT;
 
 import javax.annotation.Resource;
 import java.text.ParseException;
