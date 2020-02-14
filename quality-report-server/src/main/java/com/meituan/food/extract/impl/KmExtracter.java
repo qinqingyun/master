1ca581f2dca19e1857f0d89bb0ccb99488fad32e
liyuhua <liyuhua@meituan.com>
2020-02-14 09:57:53 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/KmExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/KmExtracter.java
@@ -37,7 +37,7 @@ public class KmExtracter implements IOneWeekDataExtract {
     private LineCoverageP0Mapper lineCoverageP0Mapper;
 
 
-    private String tableHead = "<table data-bordercolor=\\\"\\\\&quot;#cccccc\\\\&quot;\\\"><tbody><tr><th data-colwidth=\\\"61\\\" width=\\\"61\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>负责QA</p></th><th data-colwidth=\\\"354\\\" width=\\\"354\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>appKey</p></th><th data-colwidth=\\\"694\\\" width=\\\"694\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>上一周覆盖率数据（取当天往前第7天的数据）</p></th><th data-colwidth=\\\"411\\\" width=\\\"411\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>本周覆盖率数据（取当天数据）</p></th><th data-colwidth=\\\"392\\\" width=\\\"392\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>本周主要工作</p></th><th data-colwidth=\\\"392\\\" width=\\\"392\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>备注</p></th></tr>";
+    private String tableHead = "<table data-bordercolor=\\\"\\\\&quot;#cccccc\\\\&quot;\\\"><tbody><tr><th data-colwidth=\\\"61\\\" width=\\\"61\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>负责QA</p></th><th data-colwidth=\\\"354\\\" width=\\\"354\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>appKey</p></th>th data-colwidth=\\\"354\\\" width=\\\"354\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>服务等级</p></th><th data-colwidth=\\\"694\\\" width=\\\"694\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>上一周覆盖率数据（取当天往前第7天的数据）</p></th><th data-colwidth=\\\"411\\\" width=\\\"411\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>本周覆盖率数据（取当天数据）</p></th><th data-colwidth=\\\"392\\\" width=\\\"392\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>本周主要工作</p></th><th data-colwidth=\\\"392\\\" width=\\\"392\\\" style=\\\"background-color: rgb(246, 246, 246);\\\"><p>备注</p></th></tr>";
     private String tableTail = "</tbody></table><p><br></p><p></p>";
     private String trHead = "<tr>";
     private String trTail = "</tr>";
@@ -46,15 +46,27 @@ public class KmExtracter implements IOneWeekDataExtract {
     private String emptyData = "<td data-colwidth=\\\"694\\\" width=\\\"694\\\" style=\\\"background-color: rgb(255, 255, 255);\\\"><p style=\\\"text-align: start;\\\"></p></td>";
     private static final String content_url = "https://km.sankuai.com/api/pages/contentId";
     private static final String url = "https://km.sankuai.com/api/pages/8045";
+    private String kmTitleHead = "";
 
     @Override
     public void extractData4Week(LocalDate firstDay, LocalDate lastDay) {
 
         String firstDayStr = firstDay.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         String lastDayStr = lastDay.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String kmTitleTail=firstDayStr+"-"+lastDayStr+"）";
 
-        String kmTitle="交易组覆盖率统计周报（"+firstDayStr+"-"+lastDayStr+"）";
+        List<String> orderMisNames = ImmutableList.of("liyuhua", "sunnaili", "buyuqi");
+        String body = combineBody(orderMisNames);
+        String kmTitleHead="交易组覆盖率统计周报（";
+        createReport(kmTitleHead+kmTitleTail, body);
 
+        List<String> orderMisNames2 = ImmutableList.of("hanye03", "qinqingyun");
+        body = combineBody(orderMisNames2);
+        kmTitleHead="业务后台组覆盖率统计周报（";
+        createReport(kmTitleHead+kmTitleTail, body);
+    }
+
+    private void createReport(String kmTitle, String body){
 
         JSONObject jsonObject = HttpUtils.doGet(content_url, JSONObject.class, ImmutableMap.of("Cookie", "com.sankuai.it.ead.citadel_ssoid=" + SsoUtils.getSsoId()));
         Long contentId = jsonObject.getLong("data");
@@ -63,17 +75,16 @@ public class KmExtracter implements IOneWeekDataExtract {
         param.put("linkedContent", "");
 
         JSONObject params = new JSONObject();
-        params.put("title", kmTitle);
-        params.put("body",combineBody());//combineBody
-        params.put("parentId", "122541012");//122541012  258410650
+
+        params.put("body",body);//combineBody
+        params.put("parentId", "258410650");//122541012  258410650
         params.put("bodyText", "测试内容" + contentId);
         params.put("contentId", contentId);
         params.put("whatUpdate", "");
 
+        params.put("title", kmTitle);
         JSONObject resp = HttpUtils.doPost(url, params.toJSONString(), JSONObject.class, ImmutableMap.of("content-type", "application/json; charset=utf-8", "Cookie", "com.sankuai.it.ead.citadel_ssoid=" + SsoUtils.getSsoId()));
 
-        String kmLink="https://km.sankuai.com/page/"+contentId;
-        System.out.println(kmLink);
 
         //   DaXiangUtils.pushToRoom(" @郭孟瑶 "+kmLink,64012858900L);
 
@@ -98,6 +109,7 @@ public class KmExtracter implements IOneWeekDataExtract {
                         last7dayCoverage = "-.-";
 
                     oneRow = tdHead + mis + tdTail +tdHead+ appkey+ tdTail
+                            +tdHead+ "P1"+ tdTail
                             +tdHead+last7dayCoverage + tdTail
                             +tdHead+yestedayCoverage + tdTail + emptyData + emptyData;
                 }
@@ -118,6 +130,7 @@ public class KmExtracter implements IOneWeekDataExtract {
                         last7dayCoverage = "-.-";
 
                     oneRow = tdHead + mis + tdTail +tdHead+ appkey+ tdTail
+                            +tdHead+ "P2"+ tdTail
                             +tdHead+last7dayCoverage + tdTail
                             +tdHead+yestedayCoverage + tdTail + emptyData + emptyData;
                 }
@@ -133,11 +146,10 @@ public class KmExtracter implements IOneWeekDataExtract {
 
 
 
-    private String combineBody(){
+    private String combineBody(List<String> orderMisNames){
         String body = "";
         body += tableHead;
 
-        List<String> orderMisNames = ImmutableList.of("liyuhua", "sunnaili", "buyuqi");
         for(String mis:orderMisNames){
             body += getCoverageData(mis);
         }
