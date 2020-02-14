7385b6002d2b1352f12d21fa9b0fd40661472014
guomengyao <guomengyao@meituan.com>
2020-02-14 16:27:33 +0800

    增添加coe日志

--- quality-report-server/src/main/java/com/meituan/food/web/CoeController.java
+++ quality-report-server/src/main/java/com/meituan/food/web/CoeController.java
@@ -3,6 +3,7 @@ package com.meituan.food.web;
 import com.meituan.food.extract.ICOEDataExtract;
 import com.meituan.food.extract.ICargoDataPushExtract;
 import com.meituan.food.mapper.CoeListP0Mapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.text.ParseException;
 
+@Slf4j
 @RestController
 @RequestMapping("/coe")
 public class CoeController {
@@ -20,12 +22,16 @@ public class CoeController {
     @Resource
     private CoeListP0Mapper coeListP0Mapper;
 
-    @Resource(name="COEPush")
+    @Resource(name = "COEPush")
     private ICargoDataPushExtract cargoDataPushExtract;
 
     @GetMapping("/update")
-    public String updateHistoryData(@RequestParam("startDate") String startDate,@RequestParam("endDate") String endDate) throws ParseException {
-        coeDataExtract.getCOEData(startDate,endDate);
+    public String updateHistoryData(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) throws ParseException {
+        try {
+            coeDataExtract.getCOEData(startDate, endDate);
+        } catch (Exception e) {
+            log.error("updateHistoryData error, startDate: {},endDate: {}", startDate, endDate, e);
+        }
         return "OK!";
     }
 
@@ -48,7 +54,7 @@ public class CoeController {
     }
 
     @GetMapping("/delete")
-    public String deleteCoe(@RequestParam("coeId") int coeId){
+    public String deleteCoe(@RequestParam("coeId") int coeId) {
         int flag = coeListP0Mapper.deleteByCoeId(coeId);
         if (flag == 1) {
             return "删除成功";
@@ -57,7 +63,7 @@ public class CoeController {
     }
 
     @GetMapping("/push")
-    public String pushCoe(){
+    public String pushCoe() {
         cargoDataPushExtract.pushData();
         return "OK!";
     }
