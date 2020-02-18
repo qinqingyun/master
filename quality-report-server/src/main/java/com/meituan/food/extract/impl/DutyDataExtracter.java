6de87f85e839cc7bcca28d1207756593bc291b98
guomengyao <guomengyao@meituan.com>
2020-02-18 14:52:35 +0800

    删除无用代码

--- quality-report-server/src/main/java/com/meituan/food/extract/impl/DutyDataExtracter.java
+++ quality-report-server/src/main/java/com/meituan/food/extract/impl/DutyDataExtracter.java
@@ -22,12 +22,10 @@ import java.util.List;
 @Component
 public class DutyDataExtracter implements IOneDayDutyDataExtract {
 
-    public static final String url="https://tool.bitefu.net/jiari/?d=";
-
-    public static final String content="周末值班QA须知\n" +
+    public static final String content = "周末值班QA须知\n" +
             "1、客服商服反馈群，有问题反馈，会根据配置自动拉群处理，值班同学需关注群内的进度。\n" +
             "2、若提报问题自动匹配错误，需值班同学手动拉相关人进群。若提报问题未自动匹配，则需要值班同学手动拉群处理，并将问题编号记录，同步给buyuqi同学\n" +
-            "3、值班同学需关注自己值班当天，群内问题的解决情况，若问题已解决，督促提报人关闭问题。\n"+
+            "3、值班同学需关注自己值班当天，群内问题的解决情况，若问题已解决，督促提报人关闭问题。\n" +
             "4、值班同学可参考各平台业务对接人引导wiki：https://km.sankuai.com/page/181829982，拉取相关同学进群，配合解决问题。";
 
     @Resource
@@ -38,49 +36,38 @@ public class DutyDataExtracter implements IOneDayDutyDataExtract {
 
     @Override
     public void extractData4Day(LocalDate day) {
-       /* String firstDayStr = day.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        String secondDayStr=day.minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        String response=HttpUtils.doGet(url+firstDayStr,String.class,ImmutableMap.of());
-        String nextDateResponse=HttpUtils.doGet(url+secondDayStr,String.class,ImmutableMap.of());*/
-       
-       Date firstDate=localDate2Date(day);
-       Date secondDate=localDate2Date(day.minusDays(1));
+
+        Date firstDate = localDate2Date(day);
+        Date secondDate = localDate2Date(day.minusDays(1));
         Integer firstDateType = holidayService.getDayHoliday(firstDate).getType();
         Integer secongDateType = holidayService.getDayHoliday(secondDate).getType();
 
-        int count=0;
+        int count = 0;
 
-        //dayStatus代表某天的状态，0：工作日；1：周末；2：法定节假日
         //1-工作日，2-休息日（包括法定休息日和周末休息日），3-法定节假日
-      /*  int dayStatus=Integer.valueOf(response);
-        int secondDayStatus=Integer.valueOf(nextDateResponse);*/
-        if (firstDateType!=1 &&  secongDateType==1){
+        if (firstDateType != 1 && secongDateType == 1) {
             count++;
-            for(int i=1;i<=100;i++){
-               // LocalDate nextDay=day.plusDays(i);
-                Integer  nextDateType = holidayService.getDayHoliday(localDate2Date(day.plusDays(i))).getType();
-               /* String nextDayStr=nextDay.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-                String nextResp=HttpUtils.doGet(url+nextDayStr,String.class,ImmutableMap.of());
-                int nextDayStatus=Integer.valueOf(nextResp);*/
-                 if (nextDateType!=1){
-                     count++;
-                 }else {
-                     break;
-                 }
+            for (int i = 1; i <= 100; i++) {
+                Integer nextDateType = holidayService.getDayHoliday(localDate2Date(day.plusDays(i))).getType();
+                if (nextDateType != 1) {
+                    count++;
+                } else {
+                    break;
+                }
             }
-            log.info("值班人员个数为："+count+"/n");
+            log.info("值班人员个数为：" + count + "/n");
             List<DutyTablePO> dutyTablePOS = dutyTablePOMapper.selectDutyPerson(count);
-            String nameList="";
-            int i=0;
+            String nameList = "";
+            int i = 0;
             for (DutyTablePO dutyTablePO : dutyTablePOS) {
-                dutyTablePOMapper.updateByMis(day.plusDays(i).format(DateTimeFormatter.ofPattern("yyyyMMdd")),dutyTablePO.getDutyMis());
-                nameList=nameList+"@"+dutyTablePO.getDutyName()+" ";
+                dutyTablePOMapper.updateByMis(day.plusDays(i).format(DateTimeFormatter.ofPattern("yyyyMMdd")), dutyTablePO.getDutyMis());
+                nameList = nameList + "@" + dutyTablePO.getDutyName() + " ";
                 i++;
             }
 
-            DaXiangUtils.pushToPerson(nameList+"轮到你们值班了\n"+content,"guomengyao");
-          /*  DaXiangUtils.pushToRoom(nameList+"轮到你们值班了\n"+content,879074L);
-            DaXiangUtils.pushToRoom("本周值班人员："+nameList,64013968876L);*/
+            DaXiangUtils.pushToPerson(nameList + "轮到你们值班了\n" + content, "guomengyao");
+            DaXiangUtils.pushToRoom(nameList + "轮到你们值班了\n" + content, 879074L);
+            DaXiangUtils.pushToRoom("本周值班人员：" + nameList, 64013968876L);
         }
     }
 
@@ -88,11 +75,11 @@ public class DutyDataExtracter implements IOneDayDutyDataExtract {
     public void pushToAdmin(LocalDate today) {
         String firstDayStr = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         List<String> nameList = dutyTablePOMapper.selectByDate(firstDayStr);
-        DaXiangUtils.pushToPerson("本周值班人员："+nameList.toString(),"buyuqi");
+        DaXiangUtils.pushToPerson("本周值班人员：" + nameList.toString(), "buyuqi");
     }
 
     public static Date localDate2Date(LocalDate localDate) {
-        if(null == localDate) {
+        if (null == localDate) {
             return null;
         }
         ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZoneId.systemDefault());
