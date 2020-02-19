8378b38325e66855c47054584eabb38100948e8e
王佳妮 <wangjiani03@meituan.com>
2020-02-19 08:45:45 +0800

    add itPipeline

--- quality-report-server/src/main/java/com/meituan/food/web/PipelineController.java
+++ quality-report-server/src/main/java/com/meituan/food/web/PipelineController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 @RestController
@@ -19,11 +20,26 @@ public class PipelineController {
     @Resource
     private IOneDayItPipelineExtract oneDayItPipelineExtract;
 
-    @GetMapping("/insert/it")
-    public String insertReleaseName(@RequestParam("date") String date) throws ParseException {
+    @GetMapping("/insert/itdate")
+    public String insertDate(@RequestParam("date") String date) throws ParseException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date dateParam = simpleDateFormat.parse(date);
         oneDayItPipelineExtract.updateItPipelineData(dateParam);
+        return "OK!";
+    }
+    @GetMapping("/insert/it")
+    public String insertFromToDate(@RequestParam("from") String start,@RequestParam("to") String end) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = simpleDateFormat.parse(start);
+        Date endDate = simpleDateFormat.parse(end);
+        Date temp = startDate;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+        while(temp.getTime()<endDate.getTime()) {
+            temp = calendar.getTime();
+            oneDayItPipelineExtract.updateItPipelineData(temp);
+            calendar.add(Calendar.DAY_OF_MONTH,1);
+        }
         return "OK!";
     }
 }
