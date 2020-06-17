7211148963ed4b5c6d0bc96471d3c96ad9d45bf8
buyuqi <buyuqi@meituan.com>
2020-06-17 10:40:58 +0800

    fix

--- src/test/java/com/meituan/qa/meishi/Hui/cases/testsuite/Atest.java
+++ src/test/java/com/meituan/qa/meishi/Hui/cases/testsuite/Atest.java
@@ -64,7 +64,7 @@ public class Atest extends TestDPLogin {
         request.setIp("10.21.21.2");
         request.setDesc("直接退款");
         request.setOperator("859811843");
-        request.setOrderId(200325928692073l);
+        request.setOrderId(4809848809887129282l);
         request.setTarget(RefundFlowTargetEnum.CUSTOM_SERVICE.getCode());
         //request.setSuperRefund(1); 超级退款
         request.setType(RefundFlowTypeEnum.AGREE.getCode());
@@ -76,7 +76,7 @@ public class Atest extends TestDPLogin {
     @Test
     public void superRefund11()  {
         // 直接发起退款
-        String orderId = "4809848809614358115";
+        String orderId = "4809848809887129282";
         HuiRefund huiRefund = HuiRefund.builder().refundFlowService(refundFlowService).orderId(Long.valueOf(orderId)).operator(dpUserIdByq).userId(Long.valueOf(dpUserIdByq)).build();
         log.info("执行退款订单id{}",orderId);
         ApplyRefundRequest applyRefundRequest = huiRefund.apply();
