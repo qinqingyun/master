d0bafc4959092f71795f822638f538ed5e183b8a
qinqingyun <qinqingyun@meituan.com>
2020-07-01 14:16:35 +0800

    add

--- quality-report-server/src/main/java/com/meituan/food/job/impl/OneHourJobImpl.java
+++ quality-report-server/src/main/java/com/meituan/food/job/impl/OneHourJobImpl.java
@@ -3,6 +3,7 @@ package com.meituan.food.job.impl;
 import com.meituan.food.extract.ICOEDataExtract;
 import com.meituan.food.extract.ICOETdDataExtract;
 import com.meituan.food.job.IOneHourJob;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Component
+@Slf4j
 public class OneHourJobImpl implements IOneHourJob {
 
     @Resource
@@ -19,8 +21,9 @@ public class OneHourJobImpl implements IOneHourJob {
     @Resource
     private ICOETdDataExtract coeDataTdExtract;
 
+
     LocalDate firstDay=LocalDate.now().minusDays(60);
-    LocalDate secondDay=LocalDate.now().minusDays(1);
+    LocalDate secondDay=LocalDate.now();
     String firstDayStr = firstDay.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     String secondDayStr = secondDay.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
@@ -28,11 +31,16 @@ public class OneHourJobImpl implements IOneHourJob {
     @Override
     public void extractData4Hour() throws ParseException {
 
+        log.info("前60天的日期为{}",firstDayStr);
+        log.info("当前的日期为{}",secondDayStr);
         coeDataExtract.getCOEData(firstDayStr,secondDayStr);
     }
 
     public void extractDataTd4Hour() throws ParseException{
 
+        log.info("前60天的日期为{}",firstDayStr);
+        log.info("当前的日期为{}",secondDayStr);
+        log.info("没有转换的当前的时间为{}",secondDay.toString());
         coeDataTdExtract.getCOETdData(firstDayStr,secondDayStr);
 
     }
