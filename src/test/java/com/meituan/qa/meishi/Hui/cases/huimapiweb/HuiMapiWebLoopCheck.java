5451264e6a1b4e3f08a55336ae987c2fdd4833c9
zhenyumin <zhenyumin@meituan.com>
2020-07-31 13:42:20 +0800

    create order-2

--- src/test/java/com/meituan/qa/meishi/Hui/cases/huimapiweb/HuiMapiWebLoopCheck.java
+++ src/test/java/com/meituan/qa/meishi/Hui/cases/huimapiweb/HuiMapiWebLoopCheck.java
@@ -3,10 +3,26 @@ package com.meituan.qa.meishi.Hui.cases.huimapiweb;
 import com.alibaba.fastjson.JSONObject;
 import com.meituan.toolchain.mario.annotation.LoopCheck;
 import com.meituan.toolchain.mario.model.ResponseMap;
+import com.meituan.toolchain.mario.util.DBCaseRequestUtil;
 
 public class HuiMapiWebLoopCheck {
     @LoopCheck(desc = "创建订单请求轮询",interval = 500, timeout = 500*10)
-    public ResponseMap createOrderLoopQuery(JSONObject request){
-        return null;
+    public ResponseMap createOrderLoopQuery(String token,String userAgent,JSONObject request){
+        // 构造request body(请求体)
+        request.getJSONObject("headers").put("pragma-token",token);
+        request.getJSONObject("headers").put("pragma-newtoken",token);
+        request.getJSONObject("headers").put("User-Agent",userAgent);
+        request.getJSONObject("headers").put("pragma-os",userAgent);
+        // 发送post请求，并获取返回结果
+        try{
+            ResponseMap response = DBCaseRequestUtil.post("env.api.meishi.hui.host",request);
+
+            if(response.getStatusCode() != 200){
+                return null;
+            }
+            return response;
+        }catch (Exception e){
+            return null;
+        }
     }
 }
--- src/test/java/com/meituan/qa/meishi/Hui/cases/huimapiweb/TestUniCashierCreateOrderAction.java
+++ src/test/java/com/meituan/qa/meishi/Hui/cases/huimapiweb/TestUniCashierCreateOrderAction.java
@@ -6,21 +6,15 @@ import com.meituan.qa.meishi.Hui.util.TestDPLogin;
 import com.meituan.qa.meishi.util.ClassAnnotation;
 import com.meituan.qa.meishi.util.MethodAnotation;
 import com.meituan.toolchain.mario.annotation.HTTPAPI;
-import com.meituan.toolchain.mario.cases.param.Api;
 import com.meituan.toolchain.mario.framework.DBDataProvider;
 import com.meituan.toolchain.mario.model.ResponseMap;
 import com.meituan.toolchain.mario.util.AssertUtil;
-import com.meituan.toolchain.mario.util.DBCaseRequestUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
 
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import scala.util.parsing.combinator.testing.Str;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,51 +23,95 @@ import java.util.List;
 @HTTPAPI(apiPath = "hui/unicashiercreateorder.bin")
 public class TestUniCashierCreateOrderAction extends TestDPLogin{
 
-    private static Long orderId;
-    private static String serializedId;
+    private HuiMapiWebLoopCheck hmwLoop = new HuiMapiWebLoopCheck();
 
     @Test(dataProvider = "dbdata", dataProviderClass = DBDataProvider.class)
-    @MethodAnotation(author = "zhenyumin", createTime = "2020-07-29",updateTime = "2020-07-29",des = "正确用例")
+    @MethodAnotation(author = "zhenyumin", createTime = "2020-07-29",updateTime = "2020-07-31",des = "正确用例-使用折扣买单")
     public void ms_c_unicashiercreateorderaction_01(JSONObject request, JSONObject expect){
 
         List<String> path_list = Arrays.asList("$.OrderId","$.SerializedId","$.PayToken","$.Tradeno");
 
         log.info("入参：", JSON.toJSONString(request));
-        // 构造request body(请求体)
-        String token = mtToken;
-        String userAgent = mtClient;
-        request.getJSONObject("headers").put("pragma-token",token);
-        request.getJSONObject("headers").put("pragma-newtoken",token);
-        request.getJSONObject("headers").put("User-Agent",userAgent);
-        request.getJSONObject("headers").put("pragma-os",userAgent);
-//        request.getJSONObject("body").put("bizorderid",bizOrderId);
-//        request.getJSONObject("body").put("bookrecordid",bizOrderId);
-        // 发送post请求，并获取返回结果
-        ResponseMap response = DBCaseRequestUtil.post("env.api.meishi.hui.host",request);
 
+        ResponseMap response = hmwLoop.createOrderLoopQuery(mtToken,mtClient,request);      // mtToken,mtClient来自继承的类TestDPLogin
         log.info("结果返回：{}",JSON.toJSONString(response));
 
         // 校验返回结果
-        // 1 检验状态码
-        AssertUtil.assertHttp200(response);
-//        orderId = response.getValueByJsonPath("$.orderId");
-//        serializedId = response.getValueByJsonPath("$.serializedId");
+       createOrderAssert(response,path_list);
+    }
 
-//        System.out.println(orderId);
-//        System.out.println(serializedId);
-//        System.out.println(ex_orderId);
-//        System.out.println(ex_serializedId);
+
+    @Test(dataProvider = "dbdata", dataProviderClass = DBDataProvider.class)
+    @MethodAnotation(author = "zhenyumin", createTime = "2020-07-31",updateTime = "2020-07-31",des = "正确用例-使用满减方案买单")
+    public void ms_c_unicashiercreateorderaction_02(JSONObject request, JSONObject expect){
+
+        List<String> path_list = Arrays.asList("$.OrderId","$.SerializedId","$.PayToken","$.Tradeno");
+        log.info("入参：", JSON.toJSONString(request));
+
+        ResponseMap response = hmwLoop.createOrderLoopQuery(mtToken,mtClient,request);      // mtToken,mtClient来自继承的类TestDPLogin
+        log.info("结果返回：{}",JSON.toJSONString(response));
+
+        // 校验返回结果
+        createOrderAssert(response,path_list);
+    }
+
+    @Test(dataProvider = "dbdata", dataProviderClass = DBDataProvider.class)
+    @MethodAnotation(author = "zhenyumin", createTime = "2020-07-31",updateTime = "2020-07-31",des = "正确用例-使用商家抵用券买单")
+    public void ms_c_unicashiercreateorderaction_03(JSONObject request, JSONObject expect){
+
+        List<String> path_list = Arrays.asList("$.OrderId","$.SerializedId","$.PayToken","$.Tradeno");
+        log.info("入参：", JSON.toJSONString(request));
+
+        ResponseMap response = hmwLoop.createOrderLoopQuery(mtToken,mtClient,request);      // mtToken,mtClient来自继承的类TestDPLogin
+        log.info("结果返回：{}",JSON.toJSONString(response));
+
+        // 校验返回结果
+        createOrderAssert(response,path_list);
+    }
+
+    @Test(dataProvider = "dbdata", dataProviderClass = DBDataProvider.class)
+    @MethodAnotation(author = "zhenyumin", createTime = "2020-07-31",updateTime = "2020-07-31",des = "正确用例-原价买单")
+    public void ms_c_unicashiercreateorderaction_04(JSONObject request, JSONObject expect){
+
+        List<String> path_list = Arrays.asList("$.OrderId","$.SerializedId","$.PayToken","$.Tradeno");
+        log.info("入参：", JSON.toJSONString(request));
+
+        ResponseMap response = hmwLoop.createOrderLoopQuery(mtToken,mtClient,request);      // mtToken,mtClient来自继承的类TestDPLogin
+        log.info("结果返回：{}",JSON.toJSONString(response));
+
+        // 校验返回结果
+        createOrderAssert(response,path_list);
+    }
+
+
+    /**
+     * 校验创建订单接口的返回结果，包括是否JSONPath是否存在、值是否为空、OrderId是否大于0
+     * @param response：接口返回结果
+     * @param path_list：需要进行校验的字段的JSONPath
+     */
+    public void createOrderAssert(ResponseMap response,List<String> path_list){
+        // 校验返回结果
+        // 0 校验返回结果不为空
+        AssertUtil.assertNotNull(response,"下单失败：返回结果为null/空");
+
+        // 1 校验状态码
+        AssertUtil.assertHttp200(response);
+
         // 2 校验实际结果与预期结果
         // 2.1 判断JSONPath是否存在
         for(String path:path_list ){
-            AssertUtil.assertJSONPathExists(response,path,"返回结果中的"+path+"字段不存在");
+            AssertUtil.assertJSONPathExists(response,path,"下单失败：返回结果中的字段不存在");
         }
 
         // 2.2 判断是否为空
         for(String path:path_list) {
-            AssertUtil.assertNotNull(response,path,"返回结果中的"+path+"为null/空");
+            AssertUtil.assertNotNull(response,path,"下单失败：返回结果中的"+path+"为null/空");
         }
 
+        // 2.3 其他判断
+        Long orderId = response.getValueByJsonPath("$.OrderId");
+        AssertUtil.assertTrue(orderId>0,"下单失败：OrderId小于0");
     }
+
 
 }
