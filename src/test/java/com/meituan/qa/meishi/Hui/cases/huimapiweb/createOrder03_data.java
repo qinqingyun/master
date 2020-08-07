eb3587dd461b2d07a398dc0197269ecaa2870c43
zhenyumin <zhenyumin@meituan.com>
2020-08-07 21:03:25 +0800

    测试范围为核心代码

---
+++ src/test/java/com/meituan/qa/meishi/Hui/cases/huimapiweb/createOrder03_data.java
@@ -0,0 +1,98 @@
+package com.meituan.qa.meishi.Hui.cases.huimapiweb;
+
+public class createOrder03_data {
+    /*
+    入参：{
+    "headers":{"Content-Type":"application/x-www-form-urlencoded"},
+    "path":"hui/unicashiercreateorder.bin",
+    "protocol":"mapi",
+    "params":{},
+    "body":{
+        "callId":"ba546f95-a181-4c3c-afac-0a44b9e0a4d9",
+        "dpdealstring":"TDq75W6+SzF7NAGLR1HoAC2uESPP9vD2aYKe/xkIOyzOZwwEwrM/dam5vzJhL8xtHptgNhf6OCF4VSgoiJqDUdeaouF2t4YITdjhOr8KpMjrZTeU7pzFs7ktNRKHDIGF",
+        "lng":"116.4876274040632",
+        "nodiscountamount":"0",
+        "useramount":"5.0",
+        "deductAmount":"0",
+        "cityid":"1",
+        "bookrecordid":"",
+        "thirdpartyordertype":"0",
+        "bizordertype":"10",
+        "cx":"i2HKpOmsirDPavelVfQBZBnfbtlzAJ8CWapmkunFTJb4XOHx9xmmE9Aub6WmKuXBKXCiSg6gc334z1uQlZurBtMOenstuq9%2Bi2PrsKN83I5pt2CmoUZkV4FBflyirF37ZStP3AxR1UBCkbbylmS%2BhEYdpXlB2NO8mbu%2Bn4je0fW9UVZGZLyiipZxVtppyPXbfLhdeWX5AaZoOqa3TpKdToP1xi0juL5LEgyLzo2qa25yGX0SwFFLpZZhlevZavIgYVJFN/RlJcYvNWgI3Y6o0IxO6x878yo3ayKJAZAR/F0oOcwVC7v7bApPJTqieUEI/NRJL%2BlWuXe3gQk%2BCo008AdR3av1j5SxGoZTUD4gBmzcL70khO7oNU5igj4c8h7mxDEZHZeRtExz%2BVGaFZoKBXPQWVNzeV7/Yn5d7QxWvhY8zSfMXvG%2B2T3ZVV5QDM8xB59VsS0y0jNxHJpvMTVJ8F%2Bwjwg13O6FXRx/d31d8Sc6nOXjDsrrkJzCLmiuwgyX7GCVnvcFXcaigVQCnUdJaWah2pAIMZEt4SW7JIv35/AF/vaqs%2BCSwmjGx2CLJiYqrAF476NEpgJpvpMpPyNMTjCk3cXZDHboE3I7eNCdCwIiCbygf3FFaqpyrIW1JhYJBRIfXEX63mW7SDXHo8ITTExsgJ/KZfbZxivmk6qlGnqJpQ9oCv9UX2Pn/YytyBvkbxWScWFtGxQPMF5wtPCVsfI2QB7JQUWfgVWENUo35KjhQlFbd515Ji8Sw1P09v9YafG0jERx19CTr7BX7F/OeNcR1McOae5Fi7USbv9m9E4Qb%2Br4F1VqBQPQYiJWc3nQW737p8S4ym1e3sFxK%2B7bHAvbKC9ahvgdlbROvgIOuneU6SXhCAZdjVPhd2poUFctcAaWy8l/IQEfgEpa%2BcwnEPYn0PhS1EaA0cwQapr8buTqR7UgRjexmk9nfBDBiaF1SYye1yMkDV6Kp%2Bbk4amf0rOdw8kqjxWXrzx9SR2Knl5lm6Ox5MmA2FSqp8xMizgxqvq7/WNxv0x7YYtjbstx2bBRKBZlJnRiIjQxiKihZrxckgp0zxgfGnga2KJsyzeht3LStjASULULYAta8xrFLwnILOahFRx8s583HtTO0HJviEdkYOrxRMfdSyECzby23InzcX8Utic36Q6JXYJug2f6nTepH/77k%2BzcP%2BiAI7W/q1C7NG9sBCvQ4R0B2oNt811fp4cApbabvoZBmgm2dWqEYKtPbz3y1TPjemzA4Rn3bzNlybf3sJYMIob6X6tTSoGiIlBn5p2pRrbGfg5S4uhfrTQaG6X61q2VrxnhfTmEMXdOjL0tuE1fCz1g4XywFGRleTUW3T2nSMlI9%2BrmemKlLVhj/5A6F9RXSrQ45P8SAKZNFj7lbbHyek75x3x7",
+        "originamount":"10",
+        "thirdpartyorderid":"",
+        "opbvipamount":"0",
+        "shopdealstring":"",
+        "shopid":"97224769",
+        "sourcetype":"0",
+        "bizorderid":"",
+        "lat":"40.00852848297142"
+        }
+    }
+     */
+    /*
+            shopdealstring=%7B%22dealGroupId%22%3A0%2C%22dealId%22%3A0%2C%22needBuyDealCount%22%3A0%2C%22useDealCount%22%3A0%2C%22couponOfferId%22%3A%5B%5D%2C%22ticketId%22%3A0%2C%22couponList%22%3A%5B%5D%7D,
+
+     */
+    /*
+    {"headers":{"Content-Type":"application/x-www-form-urlencoded"},
+    "path":"hui/unicashiercreateorder.bin",
+    "protocol":"mapi",
+    "params":{},
+    "body":{
+        "callId":"c9bed66d-5eac-4091-a809-37783764c9b4","dpdealstring":"",
+        "lng":"116.48759736507172",
+        "nodiscountamount":"0",
+        "useramount":"0.7",
+        "deductAmount":"0",
+        "cityid":"1",
+        "shopuuid":"",
+        "bookrecordid":"",
+        "thirdpartyordertype":"0",
+        "bizordertype":"10",
+        "cx":"i2HKpOmsirDPavelVfQBZH6U0Tj8wk20oZRy4EYLGouqt4/W7HuxozzVZbHMFncKQnW7Lyji%2BueOWBITWBQLboL05QYpVIFZeOOrN4tBQF0OIGZVtyxg00eNv7NNOw6sbOeb4crkl/qXZYw%2BFxK2ya6E1kjw4rtwlhm5AFLBMselja1eaIAnfR4J7nXygpVETt911oWrS%2Bb7WKOxlYh/d7jmX%2BlBSFs7b71nsWonhBMPGfklNzdqTUAVqc7QEhgRfm/fUt/qQAY/BPas6oIdBM52Pu1jIBvH2R/XQdlfnzVqYpEIKook3NaHRqLm5D8CStcMpGmdgZ4BLmSXkdyreKfWSyV0EgZUcW55amkqEyuC9B0k23a%2BKLuxxV3RD2HixE3qNfCj%2B/APlMnNCRHEoxtNu8TU9Z9TdBDgXP9jaIKZDGS95TdoFFEzgM/Y5BGyZ/DSd469cTBt0MrHuJK8fzmVVAf3Q%2BoQeJxFcL/LzarK0eeFerXGZ60G8IZ3dIKaBQ2VWVvJc3VPqQSP6IQYkJxFglhk%2B8zPo3s5nAolyuyoS1qaqvh5D6Wzi6zebHxwyL9nOSvUhx4u58p/LD0UvIUQAR59j0tzqsaFm7VyhR4JO8WaU4B%2BNCDmG%2B2pU2n6FDFcR8WHC5OecARuC1L6O7NjjOsTCAycGO3%2B9N7qzm685mhRHd4OqIl64X9f0FZYlKsGbYULcbZi6dHFzKgc4l%2B3Sm5Clbovd1c2HFLp/PcYCaLboizBkcgnDASKwAdcl1KQMzWm7saw7Yez6QTI5j4UfiZl630yMr5NzqQeEeK/U4CeGeaJ3CkVdL0AJ6hdlN1vEFTKY2Ir7Gpga6ueQfkXYreOoSrtGaql0/yaoO1Jm5/s9AbNRsXkcXrrB2LXrBNpMpawqBLRCVxLcLI70M74vBSQfviECPWbBuQVfSiTdkCeEVHjE7d%2BYQX%2BOvSWRWqqa/fdUAwtUAdpeX3w/od5ri44dl5lihtPjkMCmSSLrRABzqORClod3Sn6OJDFUPx2IQ5uCqnoWgAXX9/SUZdiJJ10RuS51BHEetzfNXa6E1lQMGCbNmGlzn/d7mIs5mf7MfovKB0QWIZ7Y8mHxN3PI39LBO3Ow6PclGMK%2BWyAjPyOubO61nzaKmXe5o4h4skuL5Q5tQe4nJL8W7jYQdDnpAuYKdl9CnzMvMhprEi8rlfRO0T292PqB%2B9O%2Bwk86/QXoShmIwO7AlA6YU%2BDQnaRsS19ju5bDuRi99PfeVA9btLB8JDhcLtjv88ETpzqdxrpnIIPRQ3I97baCZ0n%2BMgcP%2BSDXJi0NucMXmQz33xuS/sMnxFvf5DwC9Fj0ggN93cwT/A4Ssw1w2VE9U7JysUuIqEEajwmTRz16D4jxKHHML9PYpaTork9Jsn8kyYf","originamount":"1","thirdpartyorderid":"","opbvipamount":"0","shopdealstring":"%7B%22dealGroupId%22%3A0%2C%22dealId%22%3A0%2C%22needBuyDealCount%22%3A0%2C%22useDealCount%22%3A0%2C%22couponOfferId%22%3A%5B10648207%5D%2C%22ticketId%22%3A0%2C%22couponList%22%3A%5B%7B%22ticketId%22%3A%220%22%2C%22productType%22%3A%22201%22%2C%22couponId%22%3A%2210648207%22%7D%5D%7D",
+        "shopid":"24799161",
+        "sourcetype":"0",
+        "bizorderid":"",
+        "lat":"40.00858272645521"}}
+     */
+    /*
+    入参：{headers={Content-Type=application/x-www-form-urlencoded}, path=hui/unicashiercreateorder.bin, protocol=mapi, params={}, body={callId=ba546f95-a181-4c3c-afac-0a44b9e0a4d9, dpdealstring=TDq75W6+SzF7NAGLR1HoAC2uESPP9vD2aYKe/xkIOyzOZwwEwrM/dam5vzJhL8xtHptgNhf6OCF4VSgoiJqDUdeaouF2t4YITdjhOr8KpMjrZTeU7pzFs7ktNRKHDIGF, lng=116.4876274040632, nodiscountamount=0, useramount=5.0, deductAmount=0, cityid=1, bookrecordid=, thirdpartyordertype=0, bizordertype=10, cx=123, originamount=10, thirdpartyorderid=, opbvipamount=0, shopdealstring=%7B%22dealGroupId%22%3A0%2C%22dealId%22%3A0%2C%22needBuyDealCount%22%3A0%2C%22useDealCount%22%3A0%2C%22couponOfferId%22%3A%5B%5D%2C%22ticketId%22%3A0%2C%22couponList%22%3A%5B%5D%7D, shopid=97224769, sourcetype=1, bizorderid=, lat=40.00852848297142}}
+    package com.meituan.qa.meishi.Hui.cases.huimapiweb;
+
+public class createOrder03_successData {
+        /*
+    request:{
+    headers={
+        pragma-newtoken=e09fefa697dae793ae8979c98347d5c04344ca7305c72ebee451661f866238450f8c635fa624a91fa033c18afc7c1ad2d765815df85585b1edd0cf89a1bc2b93b75b08c19f5f61d139724f01a69e5699,
+        pragma-token=e09fefa697dae793ae8979c98347d5c04344ca7305c72ebee451661f866238450f8c635fa624a91fa033c18afc7c1ad2d765815df85585b1edd0cf89a1bc2b93b75b08c19f5f61d139724f01a69e5699,
+        pragma-os=MApi 1.1(dpscope 10.16.0 appstore; iPhone 12.3.2 iPhone10,2; a0d0),
+        User-Agent=MApi 1.1(dpscope 10.16.0 appstore; iPhone 12.3.2 iPhone10,2; a0d0),
+        Content-Type=application/x-www-form-urlencoded
+        },
+   path=hui/unicashiercreateorder.bin,
+   protocol=mapi,
+   params={},
+   body={
+        callId=a7c0cb02-383b-4f80-a6c6-442bc0815e4b,
+        dpdealstring=H3DFhdZ%2FyRLpYl5dHH4TaSigZoelxM%2BUCgSx6Fc1%2FFPOKnTm7SchCLs1f%2BXvVdSRnbtZYF2t06g3tQK27F2aBacIifXx%2FXoMDyLvOSUcq8YvKIsN83SIH5eVYAiYNwQft3YZ3IhXHerKXmq2D2Wkh8fwTB18R65BZwAziOvUYZ0%3D,
+        lng=116.41358414976789,
+        nodiscountamount=0,
+        useramount=15.0,
+        deductAmount=0,
+        cityid=2,
+        bookrecordid=,
+        thirdpartyordertype=0,
+        bizordertype=10,
+        cx=123,
+        originamount=20,
+        thirdpartyorderid=,
+        opbvipamount=0,
+        shopdealstring=%7B%22dealGroupId%22%3A0%2C%22dealId%22%3A0%2C%22needBuyDealCount%22%3A0%2C%22useDealCount%22%3A0%2C%22couponOfferId%22%3A%5B%5D%2C%22ticketId%22%3A0%2C%22couponList%22%3A%5B%5D%7D,
+        shopid=65731456,
+        sourcetype=0,
+        bizorderid=,
+        lat=40.105003423783586}}
+     */
+
+}
