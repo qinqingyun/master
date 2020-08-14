5bf6e5d2657992abb3d3685412414f1a799ae063
guomengyao <guomengyao@meituan.com>
2020-08-14 21:39:47 +0800

    fix

--- quality-report-server/src/main/java/com/meituan/food/job/impl/AtpPushJobImpl.java
+++ quality-report-server/src/main/java/com/meituan/food/job/impl/AtpPushJobImpl.java
@@ -2,9 +2,11 @@ package com.meituan.food.job.impl;
 
 import com.meituan.food.extract.IAtpPushExtract;
 import com.meituan.food.job.IAtpPushJob;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
+@Component
 public class AtpPushJobImpl implements IAtpPushJob {
 
     @Resource
