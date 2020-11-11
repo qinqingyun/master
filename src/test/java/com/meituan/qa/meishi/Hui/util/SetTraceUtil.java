58d87b4d97735db5ef9144ad73d629de17306100
zhenyumin <zhenyumin@meituan.com>
2020-11-11 11:22:15 +0800

    补充mock配置的注释说明

--- src/test/java/com/meituan/qa/meishi/Hui/util/SetTraceUtil.java
+++ src/test/java/com/meituan/qa/meishi/Hui/util/SetTraceUtil.java
@@ -11,6 +11,14 @@ import lombok.extern.slf4j.Slf4j;
  * 2、新为主退款回调mock：服务：com.sankuai.mptrade.domainproxy，接口：ITradeOrderThrift.applyRefund，mock特征值：trace.get("MOCK_APPLY_REFUND")=="TRUE"
  * 3、新为主退款结算mock：服务：com.sankuai.mptrade.domainproxy，接口：ClearCommandService.getSettleAccountInfo，mock特征值：trace.get("MOCK_REFUND_SettleAccount")=="TRUE"
  * 4、单写新退款结算mock：服务：com.sankuai.mptrade.refund，接口：ClearCommandService.getSettleAccountInfo，mock特征值：trace.get("MOCK_ONLYNEWREFUND_SettleAccount")=="TRUE"
+ * 5、新为主场景，买单接入交易平台pr流程的用例配置结算mock：
+ *          服务：com.sankuai.travel.dsg.athena，
+ *          接口：IPartnerInvoicePlatService.queryPartnerInvoiceInfoAndSettleInfo，
+ *          mock特征值：trace.get('SettleMock') == true
+ * 6、单写新场景，买单接入交易平台pr流程的用例配置结算mock：
+ *          服务：com.sankuai.travel.dsg.athena，
+ *          接口：IPartnerInvoicePlatService.queryPartnerInvoiceInfoAndSettleInfo，
+ *          mock特征值：trace.get('SettleMock') == true
  */
 
 @Slf4j
@@ -23,11 +31,11 @@ public class SetTraceUtil extends TestBase {
             case "NEW_MAIN":
                 Tracer.putContext("MOCK_APPLY_REFUND","TRUE");
                 Tracer.putContext("MOCK_REFUND_SettleAccount","TRUE");
-                Tracer.putContext("SettleMock", "true");            // 各个BP接入平台pr流程的用例全部配置结算mock
+                Tracer.putContext("SettleMock", "true");            // 买单接入平台pr流程的用例全部配置结算mock
                 break;
             case "NEW_ONLY":
                 Tracer.putContext("MOCK_ONLYNEWREFUND_SettleAccount","TRUE");
-                Tracer.putContext("SettleMock", "true");              // 各个BP接入平台pr流程的用例全部配置结算mock
+                Tracer.putContext("SettleMock", "true");              // 买单接入平台pr流程的用例全部配置结算mock
                 break;
         }
     }
